608f0d209f513eac474ff6dff54859a135fdbe1d
Jared Adam <jared.adam@student.montana.edu>
2025-08-28 09:34:26 -0600

    gh_1 clean

--- Scripts/wrangling.r
+++ Scripts/wrangling.r
@@ -9,5 +9,19 @@ gh_2 <- X2025_4_17_GH_pilot2_JSA
 
 # looksy ####
 
-gh_1
+gh_1 %>% 
+  mutate(across(everything(),(~replace(.,.== 'na', NA)))) %>% 
+  mutate(date = as.Date(date, "%m/%d/%Y")) %>% 
+  mutate_at(vars(8:9), as.numeric) %>% 
+  mutate(mass_g = rowSums(.[8:9], na.rm = T)) %>% 
+  filter(diameter_mm != 'NA') %>% 
+  mutate_at(vars(2:3), as.factor) %>% 
+  mutate_at(vars(4:5), as.numeric) %>% 
+  mutate(growth = as.factor(gs)) %>% 
+  select(!c(g_full, g_spec, gs)) %>% 
+  relocate(date, trt, plant, growth) %>% 
+  print(n = Inf)
+  
+
+
 gh_2

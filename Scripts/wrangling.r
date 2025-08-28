7f8cf344b1118dc4aa817188c22a689620ffffd5
Jared Adam <jared.adam@student.montana.edu>
2025-08-28 10:26:09 -0600

    all clean

--- Scripts/wrangling.r
+++ Scripts/wrangling.r
@@ -9,7 +9,7 @@ gh_2 <- X2025_4_17_GH_pilot2_JSA
 
 # looksy ####
 
-gh_1 %>% 
+gh_1_clean <- gh_1 %>% 
   mutate(across(everything(),(~replace(.,.== 'na', NA)))) %>% 
   mutate(date = as.Date(date, "%m/%d/%Y")) %>% 
   mutate_at(vars(8:9), as.numeric) %>% 
@@ -20,8 +20,25 @@ gh_1 %>%
   mutate(growth = as.factor(gs)) %>% 
   select(!c(g_full, g_spec, gs)) %>% 
   relocate(date, trt, plant, growth) %>% 
+  print(n = 50)
+
+gh_2_clean <- gh_2 %>% 
+  mutate(across(everything(),(~replace(.,.== 'na', NA)))) %>% 
+  mutate(date = as.Date(date, "%m/%d/%Y")) %>% 
+  relocate(date, dad, trt, stage, plant) %>% 
+  mutate_at(vars(2:6), as.factor) %>% 
+  mutate_at(vars(7:10), as.numeric) %>% 
+  print(n = 50)
+
+unique(gh_2_clean$date)
+
+gh_2_mass <- gh_2_clean %>% 
+  filter(date == '2025-05-19') %>% 
+  select(!c(dad, stage, diameter_mm, leaf_count, fruit_count)) %>% 
   print(n = Inf)
-  
 
-
-gh_2
+gh_2_fitness <- gh_2_clean %>% 
+  filter(date != '2025-05-19') %>% 
+  select(!mass_g) %>% 
+  print(n = 10)
+unique(gh_2_fitness$date)
